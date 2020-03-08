e5e57f2f93ee77ac9b86797f363b9553ed81afbc
pnagy <peternagyxx@gmail.com>
2020-03-08 17:55:18 +0100

    handle missing articles

--- src/components/pages/Article/index.jsx
+++ src/components/pages/Article/index.jsx
@@ -3,10 +3,11 @@ import { withRouter } from 'react-router'
 import FullArticle from 'components/shared/FullArticle'
 import BackToList from 'components/shared/BackToList'
 
-const Article = ({ history, location: { state } }) => (
+const Article = ({ history, location: { state: article } }) => (
   <React.Fragment>
     <BackToList onClick={history.goBack} />
-    <FullArticle {...state.article}></FullArticle>
+    {!article && <h1>No article selected... :(((((</h1>}
+    {article && <FullArticle {...article}></FullArticle>}
   </React.Fragment>
 )
 
